5240b18745375540ca63155bc138daee969b42ab
Manuel A. Vázquez <manuavazquez@gmail.com>
2021-04-20 15:01:53 +0200

    - back to python version 3.6

--- make_conda_environment.sh
+++ make_conda_environment.sh
@@ -12,20 +12,11 @@ NAME=py2gift
 # only required if "anaconda" is not in the path
 source $HOME/anaconda3/etc/profile.d/conda.sh
 
-$MANAGER create --yes -n $NAME python=3 jupyterlab=2 numpy pandas ruamel.yaml tqdm pyyaml paramiko colorama nbconvert<6 nbdev -c defaults -c conda-forge -c fastai
+$MANAGER create --yes -n $NAME python=3 jupyterlab=2 ipywidgets numpy pandas ruamel.yaml tqdm pyyaml paramiko colorama twine nbconvert"<6" nbdev -c defaults -c conda-forge -c fastai
 
 echo -e new environment is \"$COLOR$NAME$UNCOLOR\"
 
 conda activate $NAME
-
-# for uploading to pypi
-pip install twine
-
-# just for testing purposes
-$MANAGER install --yes sympy
-
-# required by tqdm (progress bar in a notebook)
-$MANAGER install --yes ipywidgets
 
 # so that git is aware of nbdev/notebooks (only required once, when creating the repository, not through re-installs of conda)
 nbdev_install_git_hooks
